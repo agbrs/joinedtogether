a2d65ad491a35510a1b839b9d7cebb6d75c16b47
Corwin Kuiper <corwin@kuiper.dev>
2021-06-13 18:09:13 +0100

    add information to binary

--- release.sh
+++ release.sh
@@ -7,7 +7,7 @@ rm -rf traget/final-zip/the-hat-chooses-the-wizard
 cp -rv release-data/the-hat-chooses-the-wizard target/final-zip
 
 arm-none-eabi-objcopy -O binary target/thumbv4t-none-eabi/release/joinedtogether target/final-zip/the-hat-chooses-the-wizard/thehatchoosesthewizard.gba
-gbafix -p target/final-zip/the-hat-chooses-the-wizard/thehatchoosesthewizard.gba
+gbafix -p -tHATWIZARD -cHATW -mGC target/final-zip/the-hat-chooses-the-wizard/thehatchoosesthewizard.gba
 
 cp screenshot.png target/final-zip/the-hat-chooses-the-wizard
 
